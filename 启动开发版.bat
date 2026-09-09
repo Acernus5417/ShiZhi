0a9833265a47328bf1f7bfc8da3c3d053a174592
Acernus <minecraftyq94@gmail.com>
2026-09-10 00:31:00 +0800

    Delete 启动开发版.bat

--- 启动开发版.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0.."
-if not exist "node_modules\electron\dist\electron.exe" (
-  echo [shizhi] Electron not found. Please run: npm install
-  pause
-  exit /b 1
-)
-start "" "node_modules\electron\dist\electron.exe" "%~dp0."
